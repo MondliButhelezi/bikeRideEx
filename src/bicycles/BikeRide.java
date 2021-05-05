136482ef6f6ec6c89d346c35356472b42d77c87f
Mondli Buthelezi <67345545+MondliButhelezi@users.noreply.github.com>
2021-05-05 16:39:34 +0200

    Add files via upload
    Adding more bike rides

--- src/bicycles/BikeRide.java
+++ src/bicycles/BikeRide.java
@@ -1,62 +1,6 @@
 package bicycles;
 
-public class BikeRide implements Bicycle {
+public interface BikeRide {
 
-    private int speed = 0;
-
-    public BikeRide(Bicycle bicycle) {
-        this.bicycle = bicycle;
-    }
-
-    @Override
-    public void accelerate() {
-        speed += 5;
-    }
-
-    @Override
-    public void brake() {
-        speed -= 3;
-    }
-
-    @Override
-    public int currentSpeed() {
-        System.out.println("Your current speed is " + speed + " km/h");
-        return speed;
-    }
-
-    @Override
-    public void stop() {
-        speed = 0;
-    }
-
-    public final Bicycle bicycle; //Importing object "bicycle" from class Bicycle
-//
-//    public BikeRide(Bicycle newRide) {
-//        this.bicycle = newRide;
-//    }
-////
-////    public int currentSpeed() {
-////        System.out.println(this.bicycle.currentSpeed());
-////        return this.bicycle.currentSpeed();
-////    }
-
-//    creating a constructor that has bicycle object
-    public void ride() {
-        this.bicycle.accelerate();
-        this.bicycle.accelerate();
-        this.bicycle.accelerate();
-        this.bicycle.brake();
-        this.bicycle.brake();
-        this.bicycle.accelerate();
-    }
-
-    public static void main (String [] args) {
-//        Bicycle bicycle = new Bicycle();
-//        BikeRide bikeRide = new BikeRide(bicycle);
-//        bikeRide.ride();
-//
-//        System.out.println(bikeRide.currentSpeed());
-    }
-
-
+    void ride(Bicycle bicycle);
 }
---
+++ src/bicycles/rides/BikeRideThree.java
@@ -0,0 +1,15 @@
+package bicycles.rides;
+
+import bicycles.Bicycle;
+import bicycles.BikeRide;
+
+public class BikeRideThree implements BikeRide {
+    public final Bicycle bicycle;
+
+    public BikeRideThree (Bicycle bicycle) {
+        this.bicycle = bicycle;
+    }
+
+    @Override
+    public  void
+}
